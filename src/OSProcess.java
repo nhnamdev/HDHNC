f5273751f97114a51e204c186014f526f6220b3a
nhnamdev <nhnam23304@gmail.com>
2024-03-17 23:05:13 +0700

    Test commit with Eclipse

---
+++ src/OSProcess.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+
+public class OSProcess{
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println("Usage: java OSProcess <command>");
+			System.exit(0);
+		}
+
+		// args[0] is the command
+		ProcessBuilder pb = new ProcessBuilder(args[0]);
+		Process process = pb.start();	
+
+		// obtain the input and output streams
+		InputStream is = process.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		String line;
+		while ( (line = br.readLine()) != null)
+			System.out.println(line);
+
+		br.close();
+		System.out.println("Nam ");
+	}
+}
